dccb5fd7d9035ede830d64efbcdbd75b25d81bb1
Fredrik Dyrvold <fredrik@dyrvold.dev>
2021-05-31 09:06:36 +0200

    Reformat stacked ifs to switch/case

--- src/app/providers/providers.service.ts
+++ src/app/providers/providers.service.ts
@@ -3,15 +3,12 @@ import { Injectable } from '@angular/core';
 import { Subject, Observable } from 'rxjs';
 import { FormGroup } from '@angular/forms';
 import {
-  MainImport as GitlabMainImport,
   DefaultService as GitlabService,
 } from 'import-gitlab-client';
 import {
-  MainImport as GitHubMainImport,
   DefaultService as GitHubService,
 } from 'import-github-client';
 import {
-  MainImport as AzureDevOpsMainImport,
   DefaultService as AzureDevOpsService,
 } from 'import-azuredevops-client';
 import { ProviderType } from './provider-type.enum';
@@ -34,34 +31,32 @@ export class ProvidersService {
   }
 
   refreshProject(project: WharfProject): Observable<any> {
-    if (project.provider.name === ProviderType.GitLab.toLowerCase()) {
-      const importData: GitlabMainImport = {
-        url: project.provider.url,
-        tokenId: project.provider.tokenId,
-        group: project.groupName,
-        project: project.name,
-        providerId: project.provider.providerId
-      };
-      return this.gitlabService.gitlabPost(importData);
-    } else if (project.provider.name === ProviderType.GitHub.toLowerCase()) {
-      const importData: GitHubMainImport = {
-        url: project.provider.url,
-        tokenId: project.provider.tokenId,
-        group: project.groupName,
-        project: project.name,
-        providerId: project.provider.providerId,
-        uploadUrl: project.provider.uploadUrl,
-      };
-      return this.gitHubService.githubPost(importData);
-    } else if (project.provider.name === ProviderType.AzureDevOps.toLowerCase()) {
-      const importData: AzureDevOpsMainImport = {
-        url: project.provider.url,
-        tokenId: project.provider.tokenId,
-        group: project.groupName,
-        project: project.name,
-        providerId: project.provider.providerId
-      };
-      return this.azureDevOpsService.azuredevopsPost(importData);
+    switch (project.provider.name) {
+      case ProviderType.GitLab.toLowerCase():
+        return this.gitlabService.gitlabPost({
+          url: project.provider.url,
+          tokenId: project.provider.tokenId,
+          group: project.groupName,
+          project: project.name,
+          providerId: project.provider.providerId
+        });
+      case ProviderType.GitHub.toLowerCase():
+        return this.gitHubService.githubPost({
+          url: project.provider.url,
+          tokenId: project.provider.tokenId,
+          group: project.groupName,
+          project: project.name,
+          providerId: project.provider.providerId,
+          uploadUrl: project.provider.uploadUrl,
+        });
+      case ProviderType.AzureDevOps.toLowerCase():
+        return this.azureDevOpsService.azuredevopsPost({
+          url: project.provider.url,
+          tokenId: project.provider.tokenId,
+          group: project.groupName,
+          project: project.name,
+          providerId: project.provider.providerId
+        });
     }
   }
 }
