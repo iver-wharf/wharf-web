c661491f51e13dd19d2a393eddf8c5f40c76cc04
Fredrik Dyrvold <fredrik@struqtur.se>
2021-08-04 10:20:20 +0200

    Set oidc config for azure (dyrvold.dev)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
 
 @Component({
   selector: 'wh-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'wharf';
+
+  constructor(public oidcSecurityService: OidcSecurityService) {}
+
+  ngOnInit() {
+    this.oidcSecurityService.checkAuth().subscribe(({isAuthenticated, userData, accessToken, idToken}) => {
+      /*...*/
+      debugger;
+    });
+  }
+
+  public login() {
+    this.oidcSecurityService.authorize();
+  }
+
+  public logout() {
+    this.oidcSecurityService.logoff();
+  }
+
 }
