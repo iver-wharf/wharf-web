5b5745e35ae5617a22856c50b31ea5d6a6e8137a
Alexamakans <fougner.alexander@gmail.com>
2021-08-23 11:13:28 +0200

    Added ProblemResponse handling to GlobalErrorHandler

--- src/app/shared/error/global-error-handler.ts
+++ src/app/shared/error/global-error-handler.ts
@@ -1,5 +1,6 @@
 import { NotificationService } from './../notification/notification.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { ProblemResponse } from 'src/app/models/problem-response.model';
 import { ErrorHandler, Injectable } from '@angular/core';
 import { ErrorService } from './error.service';
 
@@ -7,18 +8,42 @@ import { ErrorService } from './error.service';
   providedIn: 'root',
 })
 export class GlobalErrorHandler implements ErrorHandler {
+  problemResponseKeys: Array<string>;
 
-    constructor(private errorService: ErrorService, private notifier: NotificationService) { }
+  constructor(private errorService: ErrorService, private notifier: NotificationService) {
+    this.problemResponseKeys = Object.keys(new ProblemResponse(
+      "detail",
+      ["error1", "error2"],
+      "instance",
+      32,
+      "title",
+      "type"
+    ));
+  }
 
-    handleError(errorResponse: Error | HttpErrorResponse) {
-        let message: string;
-
-        if (errorResponse instanceof HttpErrorResponse) {
-          message = errorResponse.message;
-        } else {
-          message = this.errorService.getClientMessage(errorResponse);
-        }
-
-        this.notifier.showError(message);
+  handleError(errorResponse: Error | HttpErrorResponse) {
+    let message: string;
+    if (errorResponse instanceof HttpErrorResponse) {
+      if (this.isProblemResponse(errorResponse.error)) {
+        this.notifier.showProblem(ProblemResponse.make(errorResponse.error));
+        return;
+      } else {
+        message = errorResponse.message;
+      }
+    } else {
+      message = this.errorService.getClientMessage(errorResponse);
     }
+    this.notifier.showError(message);
+  }
+
+  private isProblemResponse(error: any): boolean {
+    const keys = Object.keys(error);
+    for (const key of keys) {
+      if (!this.problemResponseKeys.includes(key)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 }
