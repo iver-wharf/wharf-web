e9cd3d447e3e566342323977eaa5a558e12dfbb3
Fredrik Dyrvold <fredrik@struqtur.se>
2021-10-28 12:25:32 +0200

    Set default config azure iver

--- src/app/auth/auth-config.module.ts
+++ src/app/auth/auth-config.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
-import { AuthModule, LogLevel } from 'angular-auth-oidc-client';
-// eslint-disable-next-line max-len
-import { OpenIdConfiguration } from '../../../../angular-auth-oidc-client/projects/angular-auth-oidc-client/src/lib/config/openid-configuration';
+import { AuthModule, LogLevel, OpenIdConfiguration } from 'angular-auth-oidc-client';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { WharfAuthInterceptor } from './wharf-auth.interceptor';
 
@@ -9,33 +7,32 @@ import { WharfAuthInterceptor } from './wharf-auth.interceptor';
 @NgModule({
   imports: [
     AuthModule.forRoot({
-      config: {
-        authority: 'https://login.microsoftonline.com/94999c36-9100-4153-9cbc-ddce798f5c95' +
-          '/v2.0/.well-known/openid-configuration',
-        redirectUrl: window.location.origin,
-        postLogoutRedirectUri: window.location.origin,
-        clientId: 'bf171d26-c5df-4da7-b2e6-d30f39c11863', // Application (client) ID
-        scope: 'openid profile email', //  profile email
-        responseType: 'id_token token',
-        ignoreNonceAfterRefresh: true,
-        silentRenew: true,
-        useRefreshToken: true,
-        logLevel: LogLevel.Debug,
-        maxIdTokenIatOffsetAllowedInSeconds: 600,
-        issValidationOff: false,
-        autoUserInfo: false,
-        customParamsAuthRequest: {
-          prompt: 'select_account', // login, consent
-        },
-      },
+      config: AuthConfigModule.defaultConfig,
     }),
   ],
   exports: [AuthModule],
   declarations: [],
   providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: WharfAuthInterceptor, multi: true },
+    {provide: HTTP_INTERCEPTORS, useClass: WharfAuthInterceptor, multi: true},
   ],
 })
 export class AuthConfigModule {
+  static defaultConfig: OpenIdConfiguration = {
+    authority: 'https://login.microsoftonline.com/841df554-ef9d-48b1-bc6e-44cf8543a8fc' +
+      '/v2.0/.well-known/openid-configuration',
+    redirectUrl: window.location.origin,
+    postLogoutRedirectUri: window.location.origin,
+    clientId: '01fcb3dc-7a2b-4b1c-a7d6-d7033089c779', // Application (client) ID
+    scope: 'openid profile email offline_access api://wharf-internal/read api://wharf-internal/admin api://wharf-internal/deploy',
+    responseType: 'id_token token',
+    ignoreNonceAfterRefresh: true,
+    silentRenew: true,
+    useRefreshToken: true,
+    logLevel: LogLevel.Error,
+    maxIdTokenIatOffsetAllowedInSeconds: 600,
+    issValidationOff: false,
+    autoUserInfo: false,
+  };
+
 
 }
