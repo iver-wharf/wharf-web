9c72173624f76618e288b9ae2b5d3175b839012b
Fredrik Dyrvold <fredrik@struqtur.se>
2021-11-01 16:21:40 +0100

    Cleanup cors debug

--- src/app/auth/wharf-auth.interceptor.ts
+++ src/app/auth/wharf-auth.interceptor.ts
@@ -20,14 +20,11 @@ export class WharfAuthInterceptor implements HttpInterceptor {
         if (!token) {
           return next.handle(req);
         }
-        // const allowedOrigin = new URL(apiUrl).origin;
         const bearerToken = `Bearer ${token}`;
         req = req.clone({
           setHeaders: {
             /* eslint-disable @typescript-eslint/naming-convention */
             Authorization: bearerToken,
-            //'Access-Control-Allow-Credentials': 'true',
-            //'Access-Control-Allow-Origin': allowedOrigin,
             /* eslint-enable @typescript-eslint/naming-convention */
           },
           withCredentials: true,
