d44b4eccc034d63b1b04144f9e06d3868bd53925
Fredrik Dyrvold <fredrik@dyrvold.dev>
2021-12-09 19:33:09 +0100

    Remove root compoenent debug info.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { OidcSecurityService } from 'angular-auth-oidc-client';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'wh-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
   title = 'wharf';
-
-  constructor(public oidcSecurityService: OidcSecurityService) {}
-
-  ngOnInit() {
-    this.oidcSecurityService.checkAuth().subscribe(({ isAuthenticated, userData, accessToken }) => {
-      console.log('app authenticated', isAuthenticated);
-      console.log(`Current access token is '${accessToken}'`);
-    });
-  }
-
 }
