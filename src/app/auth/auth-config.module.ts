3d9f40fc21d775c804a0e346f94a8a0aa9403fc2
Fredrik Dyrvold <fredrik@dyrvold.dev>
2022-01-17 16:41:46 +0100

    Load OIDC config from assets/config

--- src/app/auth/auth-config.module.ts
+++ src/app/auth/auth-config.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
-import { AuthModule, LogLevel, OpenIdConfiguration } from 'angular-auth-oidc-client';
+import {
+  AuthModule,
+  StsConfigHttpLoader,
+  StsConfigLoader,
+} from 'angular-auth-oidc-client';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { WharfAuthInterceptor } from './wharf-auth.interceptor';
+import { ConfigService } from '../shared/config/config.service';
 
+const authFactory = (configService: ConfigService) => new StsConfigHttpLoader(configService.getOidcConfig$());
 
 @NgModule({
   imports: [
     AuthModule.forRoot({
-      config: AuthConfigModule.defaultConfig,
+      loader: {
+        provide: StsConfigLoader,
+        useFactory: authFactory,
+        deps: [ConfigService],
+      },
     }),
   ],
   exports: [AuthModule],
@@ -16,23 +26,4 @@ import { WharfAuthInterceptor } from './wharf-auth.interceptor';
     {provide: HTTP_INTERCEPTORS, useClass: WharfAuthInterceptor, multi: true},
   ],
 })
-export class AuthConfigModule {
-  static defaultConfig: OpenIdConfiguration = {
-    authority: 'https://login.microsoftonline.com/841df554-ef9d-48b1-bc6e-44cf8543a8fc' +
-      '/v2.0/.well-known/openid-configuration',
-    redirectUrl: window.location.origin,
-    postLogoutRedirectUri: window.location.origin,
-    clientId: '01fcb3dc-7a2b-4b1c-a7d6-d7033089c779', // Application (client) ID
-    scope: 'openid profile email offline_access api://wharf-internal/read api://wharf-internal/admin api://wharf-internal/deploy',
-    responseType: 'id_token token',
-    ignoreNonceAfterRefresh: true,
-    silentRenew: true,
-    useRefreshToken: true,
-    logLevel: LogLevel.Error,
-    maxIdTokenIatOffsetAllowedInSeconds: 600,
-    issValidationOff: false,
-    autoUserInfo: false,
-  };
-
-
-}
+export class AuthConfigModule {}
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {
-  OidcClientNotification,
   OidcSecurityService,
   OpenIdConfiguration,
   UserDataResult,
