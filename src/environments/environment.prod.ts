1479eccd29ca952119309386800c84c7bc0b87ec
Kalle Fagerberg <kalle.fagerberg@iver.se>
2022-04-08 10:54:31 +0200

    Added comment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,13 @@
 import { environmentDefaults } from './environment.default';
 import { Environment } from './environment.types';
 
+/**
+ * This file replaces environment.ts in production builds (via angular.json),
+ * which is selected when running `npm run build-prod`, such as in the Dockerfile.
+ *
+ * In regular `npm start` builds, this file is unused.
+ */
+
 export const environment: Environment = {
   ...environmentDefaults,
 
